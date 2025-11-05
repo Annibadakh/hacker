c02c258419c433324282f2cfd82868edf5a1170b
Aniket Badakh <aniketbadakh1212@gmail.com>
2025-11-05 11:40:29 +0530

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
 import { useEffect } from 'react'
-import React from 'react';
 
 function App() {
 
   const handleDownload = () => {
     const link = document.createElement('a');
-    link.href = '/temp.zip'; // ZIP file path in `public/`
-    link.download = 'sample.zip'; // Optional: set download name
+    link.href = '/temp.zip';
+    link.download = 'sample.zip';
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
   };
 
   useEffect(() => {
-    handleDownload();
+    // handleDownload();
   }, [])
 
   return (
-    <></>
+    <p>...</p>
   )
 }
 
