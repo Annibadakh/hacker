968e483b2cf507f2132a28a815ae61f095dad6b8
Aniket Badakh <aniketbadakh1212@gmail.com>
2025-11-09 22:21:11 +0530

    file change

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   const handleDownload = () => {
     const link = document.createElement('a');
-    link.href = '/temp.zip';
+    link.href = '/ml.zip';
     link.download = 'ml.zip';
     document.body.appendChild(link);
     link.click();
